349c0376f40e0e6bca7db45aac5e6ff2d2fb909d
Mutual <vlah.mot@gmail.com>
2013-09-20 21:35:44 -0400

    Added base Puzzle class and WordPuzzle class

--- src/Model/Puzzles/CrossWord.java
+++ src/Model/Puzzles/CrossWord.java
@@ -1,5 +1,29 @@
 package Model.Puzzles;
 
-public class CrossWord {
+import Model.Puzzles.Parts.*;
 
+import java.awt.Dimension;
+import java.util.*;
+
+public class CrossWord extends Puzzle
+{
+	HashMap<String,WordLocation> wordMap;
+	
+	CrossWordCell[][] cellArray;
+	
+	@Override
+	public char charAt(Dimension dim)
+	{
+		return cellArray[dim.width][dim.height].getChar();
+	}
+	@Override
+	public boolean addWord(String word, WordLocation location)
+	{
+		return true;
+	}
+	@Override
+	public boolean deleteWord(String word, WordLocation location)
+	{
+		return true;
+	}
 }
--- src/Model/Puzzles/Puzzle.java
+++ src/Model/Puzzles/Puzzle.java
@@ -1,6 +1,18 @@
 package Model.Puzzles;
+import java.awt.Dimension;
 
-public class Puzzle
+import Model.Puzzles.Parts.*;
+
+/**
+ * Represents a basic puzzle.
+ * @author tom
+ *
+ */
+public abstract class Puzzle
 {
-
+	public abstract char charAt(Dimension dim);
+	
+	public abstract boolean addWord(String word, WordLocation location);
+	
+	public abstract boolean deleteWord (String word, WordLocation location);
 }
