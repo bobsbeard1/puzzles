9c018e2c330f5c9f0bec396f5dcc0f066c3eb80e
vlahmot <trhlavat@millersville.edu>
2013-09-24 18:14:06 -0400

    test

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -17,7 +17,7 @@ public class Main
 	public static void main(String[] args) throws IOException
 	{
 		final GUI gui = new GUI();
-		
+		//54
 		String path = "moby_list.txt";
 		WordList wordList = new WordList(FileManager.getSubWordListFromFile(path,50));
 		wordList.printList();
