e7b47c4b348511c2287e76df19fbdbe66f25c6d8
bobsbeard1 <bobsbeard1@gmail.com>
2013-09-24 19:40:04 -0400

    Moved model data from main into GameModel class.

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import Utility.FileManager;
 import View.GUI;
+import Model.GameModel;
 import Model.WordList;
 import Model.Puzzles.*;
 import Model.Puzzles.Parts.WordLocation;
@@ -18,41 +19,11 @@ public class Main
 	{
 		final GUI gui = new GUI();
 		
-		String path = "words.txt";
-		if(path.endsWith("txt"))
-		{
-			WordList wordList = new WordList(FileManager.getWordListFromFile(path));
-			//WordList wordList = new WordList(FileManager.getSubWordListFromFile(path,50));
-			wordList.printList();
-			System.out.println(wordList.getLength());
-		}
-		else
-		{
-			//WordList wordList = new WordList(FileManager.getSubWordListFromFile(path,50));
-			WordList wordList = new WordList(FileManager.getWordListFromFile(path));
-			wordList.printList();
-			System.out.println(wordList.getLength());
-		}
-		int size = 20;
-		CrossWord word = (CrossWord)PuzzleGenerator.generatePuzzle(PuzzleType.CrossWord, size);
-		CrossWord word2 = (CrossWord)PuzzleGenerator.generatePuzzle(PuzzleType.CrossWord,size);
-		WordSearch search = (WordSearch)PuzzleGenerator.generatePuzzle (PuzzleType.WordSearch, size);
-		HashMap<String,WordLocation> map1 = new HashMap<String,WordLocation>();
-		HashMap<String,WordLocation> map2 = new HashMap<String,WordLocation>();
-		
-		map1.put("One", new WordLocation(1,2));
-		map2.put("Two",new WordLocation(2,1));
-		map2.put("Three",new WordLocation(2,1));
-		
-		word.printCrossWord();
-		
-		word.setWordMap(map1);
-		search.printWordSearch();
-		word2.setWordMap(map2);
+		GameModel model = new GameModel();
 		
 		
 		
-		gui.getPuzzleArea().setActivePuzzle(word);
+		gui.getPuzzleArea().setActivePuzzle(model.getCrossWord());
 		
 
 		SwingUtilities.invokeLater(new Runnable()
--- src/Model/GameModel.java
+++ src/Model/GameModel.java
@@ -1,5 +1,58 @@
 package Model;
 
-public class GameModel {
+import java.io.IOException;
+import java.util.HashMap;
 
+import Model.Puzzles.CrossWord;
+import Model.Puzzles.PuzzleGenerator;
+import Model.Puzzles.PuzzleType;
+import Model.Puzzles.WordSearch;
+import Model.Puzzles.Parts.WordLocation;
+import Utility.FileManager;
+
+public class GameModel 
+{
+	WordList wordList;
+	String path;
+	CrossWord crossWord;
+	WordSearch wordSearch;
+	int size;
+	
+	
+	public GameModel() throws IOException
+	{
+		size = 20;
+		crossWord = (CrossWord)PuzzleGenerator.generatePuzzle(PuzzleType.CrossWord, size);
+		wordSearch = (WordSearch)PuzzleGenerator.generatePuzzle (PuzzleType.WordSearch, size);
+		path = "words.txt";
+		if(path.endsWith("txt"))
+		{
+			WordList wordList = new WordList(FileManager.getWordListFromFile(path));
+			//WordList wordList = new WordList(FileManager.getSubWordListFromFile(path,50));
+			wordList.printList();
+			System.out.println(wordList.getLength());
+		}
+		else
+		{
+			//WordList wordList = new WordList(FileManager.getSubWordListFromFile(path,50));
+			WordList wordList = new WordList(FileManager.getWordListFromFile(path));
+			wordList.printList();
+			System.out.println(wordList.getLength());
+		}
+	}
+	
+	public WordSearch getWordSearch()
+	{
+		return wordSearch;
+	}
+	
+	public CrossWord getCrossWord()
+	{
+		return crossWord;
+	}
+	
+	
+
+	
+	
 }
