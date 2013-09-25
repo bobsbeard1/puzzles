e0785aadc7349bb4f077fd42df2a686c5af2f920
Mutual <vlah.mot@gmail.com>
2013-09-24 22:22:49 -0400

    Moved puzzle generation from cases to functions.

--- src/Model/Puzzles/PuzzleGenerator.java
+++ src/Model/Puzzles/PuzzleGenerator.java
@@ -12,45 +12,53 @@ public class PuzzleGenerator
 		switch (type)
 		{
 		case CrossWord:
-			
-			CrossWordCell[][] crosswordArray = new CrossWordCell[size][size];
-			
-			for (int i = 0; i < crosswordArray.length; i++)
-			{
-				for (int j = 0; j < crosswordArray[0].length; j++)
-				{
-					crosswordArray[i][j] = new CrossWordCell(RandomHelper.getRandomChar());
-				}
-			}
-			
-			crosswordArray[0][0].setChar('#');
-			crosswordArray[0][1].setChar('#');
-			crosswordArray[0][2].setChar('#');
-			crosswordArray[0][3].setChar('#');
-			crosswordArray[1][3].setChar('#');
-			crosswordArray[5][5].setChar('#');
-			crosswordArray[2][2].setChar('#');
-			
-			return new CrossWord(crosswordArray);
-			
+			return generateCrossWord(size);
 		case WordSearch:
-			WordSearch wordSearch = new WordSearch (size);
-			WordSearchCell[][] wordsearchArray = new WordSearchCell[size][size];
-			
-			for (int i = 0; i < wordsearchArray.length; i++)
-			{
-				for (int j = 0; j < wordsearchArray[0].length; j++)
-				{
-					wordsearchArray[i][j] = new WordSearchCell(RandomHelper.getRandomChar());
-				}
-			}
-			
-			wordSearch = new WordSearch (wordsearchArray);
-			
-			return wordSearch;
-		
+			return generateWordSearch(size);
 		}
 		return null;
+		
+	}
+	private static WordSearch generateWordSearch(int size)
+	{
+
+		WordSearch wordSearch = new WordSearch (size);
+		WordSearchCell[][] wordsearchArray = new WordSearchCell[size][size];
+		
+		for (int i = 0; i < wordsearchArray.length; i++)
+		{
+			for (int j = 0; j < wordsearchArray[0].length; j++)
+			{
+				wordsearchArray[i][j] = new WordSearchCell(RandomHelper.getRandomChar());
+			}
+		}
+		
+		wordSearch = new WordSearch (wordsearchArray);
+		
+		return wordSearch;
+	}
+	private static CrossWord generateCrossWord(int size)
+	{
+
+		CrossWordCell[][] crosswordArray = new CrossWordCell[size][size];
+		
+		for (int i = 0; i < crosswordArray.length; i++)
+		{
+			for (int j = 0; j < crosswordArray[0].length; j++)
+			{
+				crosswordArray[i][j] = new CrossWordCell(RandomHelper.getRandomChar());
+			}
+		}
+		
+		crosswordArray[0][0].setChar('#');
+		crosswordArray[0][1].setChar('#');
+		crosswordArray[0][2].setChar('#');
+		crosswordArray[0][3].setChar('#');
+		crosswordArray[1][3].setChar('#');
+		crosswordArray[5][5].setChar('#');
+		crosswordArray[2][2].setChar('#');
+		
+		return new CrossWord(crosswordArray);
 		
 	}
 }
